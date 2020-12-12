8461eb5ef0662d6bcdbba9c951a4494f877b13b6
Henry Canastuj <henryjc97@gmail.com>
2020-12-12 00:03:10 -0600

    Editar categoria agregado

--- peliculas/urls.py
+++ peliculas/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
    path('', views.post_list, name='post_list'),
    path('categorias/', views.categorias, name='categorias'),
+   path('categorias/nuevo', views.categoria_nuevo, name='categoria_nuevo'),
+   path('categoria/<int:pk>/edit/', views.categoria_edit, name='categoria_edit'),
 ]
--- peliculas/views.py
+++ peliculas/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from .models import Categoria 
+from .forms import CategoriaForm
+from django.shortcuts import redirect
+from django.shortcuts import render, get_object_or_404
 
 def post_list(request):
     return render(request, 'peliculas/base.html', {'publicacion':'publicacion'})
@@ -7,3 +10,26 @@ def post_list(request):
 def categorias(request):
     categorias = Categoria.objects.order_by('nombre')
     return render(request, 'peliculas/categorias.html', {'categorias':categorias})
+
+def categoria_nuevo(request):
+    if request.method == "POST":
+        form = CategoriaForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.save()
+            return redirect('categorias')
+    else:
+        form = CategoriaForm()
+    return render(request, 'peliculas/categoria_edit.html', {'form': form})
+
+def categoria_edit(request, pk):
+    categoria = get_object_or_404(Categoria, pk=pk)
+    if request.method == "POST":
+        form = CategoriaForm(request.POST, instance=categoria)
+        if form.is_valid():
+            categoria = form.save(commit=False)
+            categoria.save()
+            return redirect('categorias')
+    else:
+        form = CategoriaForm(instance=categoria)
+    return render(request, 'peliculas/categoria_edit.html', {'form': form})
