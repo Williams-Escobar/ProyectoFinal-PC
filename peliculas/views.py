c3598877035290d3d3da30ff01835d04ec9820a4
Henry Canastuj <henryjc97@gmail.com>
2020-12-11 21:16:12 -0600

    Vista para ver categorias creada

--- peliculas/views.py
+++ peliculas/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from .models import Categoria 
 
 def post_list(request):
     return render(request, 'peliculas/base.html', {'publicacion':'publicacion'})
+
+def categorias(request):
+    categorias = Categoria.objects.order_by('nombre')
+    return render(request, 'peliculas/categorias.html', {'categorias':categorias})
