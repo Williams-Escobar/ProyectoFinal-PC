eb3e3fae669ed56b6923997b534402c247e30636
Williams Escobar <alexander.juarez@mesoamericana.edu.gt>
2020-12-12 11:32:56 -0600

    CRUD y rutas Finalizado

--- peliculas/urls.py
+++ peliculas/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-   path('', views.post_list, name='post_list'),
    path('categorias/', views.categorias, name='categorias'),
    path('categorias/nuevo', views.categoria_nuevo, name='categoria_nuevo'),
    path('categoria/<int:pk>/edit/', views.categoria_edit, name='categoria_edit'),
