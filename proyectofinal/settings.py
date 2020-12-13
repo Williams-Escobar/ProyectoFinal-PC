e85c1b4f4d889f46df701147e567fc2da832753f
Henry Canastuj <henryjc97@gmail.com>
2020-12-13 02:27:54 -0600

    Prueba de despligue automatico 7

--- proyectofinal/settings.py
+++ proyectofinal/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '+l@a=zd(@rv8+c2_72!li!9dvq=uk4d%ic#eftd2+hk$(a+nd%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
