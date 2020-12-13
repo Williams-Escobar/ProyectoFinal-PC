4b26423d89858c871b8a40423343992cfd35dac1
Williams Escobar <alexander.juarez@mesoamericana.edu.gt>
2020-12-13 02:24:26 -0600

    Subir reproduccion Automatico

--- peliculas/views.py
+++ peliculas/views.py
@@ -7,6 +7,18 @@ from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 import git
 
+@csrf_exempt
+def update(request):
+    if request.method == "POST":
+        repo = git.Repo("henryjosue.pythonanywhere.com/") 
+        origin = repo.remotes.origin
+
+        origin.pull()
+
+        return HttpResponse("Updated code on PythonAnywhere")
+    else:
+        return HttpResponse("Couldn't update the code on PythonAnywhere")
+
 def post_list(request):
     return render(request, 'peliculas/base.html', {'publicacion':'publicacion'})
 
@@ -79,15 +91,3 @@ def delete_pelicula(request, pk):
     pelicula = get_object_or_404(Pelicula, pk=pk)
     pelicula.delete()
     return redirect('pelicula_list')
-
-@csrf_exempt
-def update(request):
-    if request.method == "POST":
-        repo = git.Repo("henryjosue.pythonanywhere.com/") 
-        origin = repo.remotes.origin
-
-        origin.pull()
-
-        return HttpResponse("Updated code on PythonAnywhere")
-    else:
-        return HttpResponse("Couldn't update the code on PythonAnywhere")
