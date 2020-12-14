c8d6fd77aae2981551ccda6ae1ddee7894e0741e
Henry Canastuj <henryjc97@gmail.com>
2020-12-13 19:54:27 -0600

    Prueba 2

--- peliculas/models.py
+++ peliculas/models.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 # Create your models here.
 class Categoria (models.Model):
     nombre = models.CharField(max_length=50)
-    descripcion = models.IntergerField(max_length=100)
+    descripcion = models.CharField(max_length=100)
 
     def __str__(self):
         return self.nombre 
